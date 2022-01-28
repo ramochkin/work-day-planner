2811ac1768bee181227d52857677c5ba2c0d48a3
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-01-28 16:20:14 -0500

    final copy

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+$("#currentDay").text(moment().format('MMMM Do YYYY'))
 $(".saveBtn").on('click', function(){
     var inputText = $(this).siblings('.description').val();
     var inputTime = $(this).parent().attr('id');
@@ -7,9 +7,40 @@ $(".saveBtn").on('click', function(){
 
 
 
+//change id's to military time HTML
 
+//write function comparing timeblock hour to current time.
+//if past add class past, if future add class future, if present add class present
+//Google Loops in Jquery
+function styling(){
+ var currentHour = moment().hours();
 
+    $('.time-block').each(function(){
+        var rowhour = parseInt($(this).attr('id'))
 
+        if(rowhour < currentHour){
+            //we need to add a class of past
+            $(this).addClass('past')
+        } else if(rowhour === currentHour) {
+            $(this).addClass('present')
+            $(this).removeClass('past')
+        } else {
+            $(this).addClass('future')
+            $(this).removeClass('past')
+            $(this).removeClass('present')
+        }
+    })
+}
 
+styling()
 
-//moment().hours()
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
+
